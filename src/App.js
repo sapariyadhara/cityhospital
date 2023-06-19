4db196db8be52b1530dada8cd177bec6bf2c3dcf
sapariyadhara <sapariyadhara935@gmail.com>
2023-06-19 21:59:59 +0530

    add pages with react router dom

--- src/App.js
+++ src/App.js
@@ -1,15 +1,30 @@
 
-import Footer from './components/Footer';
-import Header from './components/Header';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import React from 'react';
+import About from './containers/About';
 import Home from './containers/Home';
+import Appoinment from './containers/Appoinment';
+import Contect from './containers/Contect';
+import Departments from './containers/Departments';
+import Doctors from './containers/Doctors';
+
+
+
 
 function App() {
   return (
-    <div>
-     <Header />
-     <Home />
-     <Footer />
-    </div>
+  
+     <Router>
+      <Routes>   
+        <Route path='/Home' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/Appoinment' element={<Appoinment />} />
+        <Route path='/Contect' element={<Contect />} />
+        <Route path='/Departments' element={<Departments />} />
+        <Route path='/Doctors' element={<Doctors />} />
+      </Routes>
+     </Router>    
+     
   );
 }
 
