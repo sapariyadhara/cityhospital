d2a7189c28bdf2b2f25b4faa9c78929f3b7b3f72
sapariyadhara <sapariyadhara935@gmail.com>
2023-09-04 11:27:24 +0530

    add medicine data

--- src/users/medicines/Medicines.js
+++ src/users/medicines/Medicines.js
@@ -1,18 +1,15 @@
 import React, { useContext, useEffect, useState } from "react";
 import ListMedicines from "./ListMedicines";
 import { useDispatch, useSelector } from "react-redux";
-import { getData } from "../../redux/action/medicine.action";
-import { Row } from "reactstrap";
-// import { addToCart } from "../../redux/action/cart.action";
-import { addToFav, removeToFav } from "../../redux/action/myfav.action";
+import { addToFav } from "../../redux/action/myfav.action";
 import { addToCart } from "../../redux/slice/cartSlice";
 import { ThemeContext } from "../../Context/ThemeContext";
-import { hover } from "@testing-library/user-event/dist/hover";
+import { getMedicData } from "../../redux/slice/medicineSlice";
 
 function Medicines(props) {
   const theme = useContext(ThemeContext)
   const dispatch = useDispatch();
-  const mediUser = useSelector((state) => state.Medicine);
+  const mediUser = useSelector((state) => state.medicinef);
   const mFav = useSelector((state) => state.myfav)
   console.log(mFav);
   console.log(mediUser, "userM");
@@ -21,13 +18,8 @@ function Medicines(props) {
   const [filterData, setFilterData] = useState([]);
 
   useEffect(() => {
-    // let localData = JSON.parse(localStorage.getItem('medicine'))
-    // if(localData){
-    //     setData(localData)
-    // }
-
-    //get data with redux
-    dispatch(getData());
+   
+    dispatch(getMedicData());
   }, []);
 
   const handleChange = (val) => {
@@ -91,7 +83,7 @@ function Medicines(props) {
         </div>
         <div class="row">
           <ListMedicines 
-          mdata={filterData.length > 0 ? filterData : mediUser.medicineD} 
+          mdata={filterData.length > 0 ? filterData : mediUser.medic} 
           handleCart1={handleCart} 
           handleAddToFav={handleAddToFav}
           favData={mFav}
